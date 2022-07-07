a036a696ba23e73a28db133851a11e296f30fc40
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-07-07 20:42:38 +0530

    Update Battery_Health_Test.cpp

--- Battery_Health_Test.cpp
+++ Battery_Health_Test.cpp
@@ -4,6 +4,6 @@
 int main()
 {
 BatteryChecker BatteryStatusCheck;
-    assert(BatteryStatusCheck.batteryIsOk(25, 70, 0.7) == true);
-    assert(BatteryStatusCheck.batteryIsOk(50, 85, 0) == false);
+    assert(BatteryStatusCheck.batteryHealthIsOk(25, 70, 0.7) == true);
+    assert(BatteryStatusCheck.batteryHealthIsOk(50, 85, 0) == false);
 }
