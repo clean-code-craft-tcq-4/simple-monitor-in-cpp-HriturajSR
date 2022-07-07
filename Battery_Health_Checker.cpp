515f2fce3db1e38991a4776313f524cd9e0a32bc
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-07-07 20:48:16 +0530

    Update Battery_Health_Checker.cpp

--- Battery_Health_Checker.cpp
+++ Battery_Health_Checker.cpp
@@ -1,6 +1,9 @@
 #include "Battery_Health_Checker.h"
 #include "iostream"
 
+namespace battery
+{
+
   static float TEMP_MIN = 0;
   static float TEMP_MAX = 45;
   static float SOC_MIN = 20;
@@ -8,18 +11,17 @@
   static float CHARGERATE_MIN = 0;
   static float CHARGERATE_MAX = 0.8;
 
-
-bool BatteryChecker::checkRange(float actualValue, float minValue, float maxValue)
+ bool battery::BatteryChecker::checkRange(float actualValue, float minValue, float maxValue)
 {
     return (actualValue < minValue || actualValue > maxValue);
 }
 
-bool BatteryChecker::batteryIsOk(float temperature, float soc, float chargeRate)
+ bool battery::BatteryChecker::batteryIsOk(float temperature, float soc, float chargeRate)
 {
    return (batteryTemperatureIsOk(temperature) && batterySocIsOk(soc) && batteryChargeRateIsOk(chargeRate));
 }
 
-bool BatteryChecker::batteryTemperatureIsOk(float temperature)
+ bool battery::BatteryChecker::batteryTemperatureIsOk(float temperature)
 {
     bool result = true;
     if (checkRange(temperature, TEMP_MIN, TEMP_MAX))
@@ -30,7 +32,7 @@ bool BatteryChecker::batteryTemperatureIsOk(float temperature)
     return result;
 }
 
-bool BatteryChecker::batterySocIsOk(float soc)
+ bool battery::BatteryChecker::batterySocIsOk(float soc)
 {
     bool result = true;
     if (checkRange(soc, SOC_MIN, SOC_MAX))
@@ -41,7 +43,7 @@ bool BatteryChecker::batterySocIsOk(float soc)
     return result;
 }
 
-bool BatteryChecker::batteryChargeRateIsOk(float chargeRate)
+ bool battery::BatteryChecker::batteryChargeRateIsOk(float chargeRate)
 {
      bool result = true;
     if (checkRange(chargeRate, CHARGERATE_MIN, CHARGERATE_MAX))
@@ -50,4 +52,5 @@ bool BatteryChecker::batteryChargeRateIsOk(float chargeRate)
       result = false;
     }
     return result;
+}
 }
